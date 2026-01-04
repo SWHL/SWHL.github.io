ef94ec6851cbfcfba27a21b3c4e199daa862975a
SWHL <liekkaskono@163.com>
2026-01-04 11:26:48 +0800

    chore: update files

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,10 @@
+/* ==========================================
+   Zensical-Wcowin 自定义脚本
+   ========================================== */
+
+// 即时导航兼容
+document$.subscribe(function() {
+  console.log('Zensical-Wcowin loaded');
+  
+  // 在这里添加你的自定义 JavaScript
+});
