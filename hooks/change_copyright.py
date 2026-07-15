43a3d0d2edd688c2af0450c5d849945e2bd43f2d
SWHL <liekkaskono@163.com>
2026-07-15 14:27:02 +0800

    chore: update files

--- hooks/change_copyright.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-# @Author: SWHL
-# @Contact: liekkaskono@163.com
-from datetime import datetime
-
-
-def on_config(config, **kwargs):
-    config.copyright = f"Copyright &copy; {datetime.now().year} Maintained by SWHL."
